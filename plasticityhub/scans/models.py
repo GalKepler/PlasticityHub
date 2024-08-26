5c6ae2d23431aefe15ad2e83ecaad95311536444
GalKepler <galkepler@gmail.com>
2024-08-26 16:16:05 +0300

    fixed ordering of sessions

--- plasticityhub/scans/models.py
+++ plasticityhub/scans/models.py
@@ -47,17 +47,23 @@ class Session(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    datetime = models.DateTimeField(editable=False, null=True)
 
     class Meta:
         verbose_name = "Session"
         verbose_name_plural = "Sessions"
-        ordering = ["session_id"]
+        ordering = ["datetime"]
 
     def __str__(self):
         return f"Session {self.id} for {self.subject} on {self.date} at {self.time}"
 
+    def save(self, *args, **kwargs):
+        # Automatically set the inferred_datetime field
+        self.datetime = self.inferred_datetime
+        super().save(*args, **kwargs)
+
     @property
-    def session_date(self):
+    def inferred_datetime(self):
         """
         Return the datetime of the session based on the session
         """
@@ -68,11 +74,11 @@ class Session(models.Model):
         """
         Return the date of the session
         """
-        return self.session_date.date()
+        return self.inferred_datetime.date()
 
     @property
     def time(self):
         """
         Return the time of the session
         """
-        return self.session_date.time()
+        return self.inferred_datetime.time()
